feb52b17c45f075bd5db48e57564e40d639061e5
Yuliya Sidarenka <yulka1102@gmail.com>
2022-01-13 00:31:46 +0100

    Users page added, axios installed, ajax-request to server, new logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import s from './Header.module.css'
+import logo from '../../assets/images/logo.png'
 
 
 const Header = () => {
     return (
         <header className={s.header}>
             <div className={s.img}>
-                <img src="https://png.pngtree.com/png-clipart/20190810/ourlarge/pngtree-hand-painted-blue-social-hand-account-dialog-png-image_1685816.jpg" alt="pic"/>
+                <img src={logo} alt="pic"/>
             </div>
             <div>Welcome to our page</div>
         </header>
