ca1f9df95c48626a634967d52bda9270002ca751
Yuliya Sidarenka <yulka1102@gmail.com>
2022-01-22 23:36:47 +0100

    componentDidUpdate in ProfileStatus.jsx

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Routes>
-                        <Route path={`/profile`}
+                        <Route path={`/profile/*`}
                                element={<ProfileContainer />}/>
                         <Route path={`/profile/:userId/`}
                                element={<ProfileContainer />}/>
