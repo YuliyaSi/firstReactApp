cc227f6c1d555509c72a97f9b633cfb9323014ed
Yuliya Sidarenka <yulka1102@gmail.com>
2022-03-03 17:32:41 +0100

    hide api-key

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,
     headers: {
-        "API-KEY": "b8e21a22-d896-4a37-83b9-6901589c197a"
+        "API-KEY": process.env.REACT_APP_SAMURAI_API_KEY
     },
 });
 
